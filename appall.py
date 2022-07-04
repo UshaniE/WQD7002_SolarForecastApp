a266e45d3549a124e8ae86b6fe3368e2e7c81e57
UshaniE <73177920+UshaniE@users.noreply.github.com>
2022-07-04 23:23:22 +0800

    Add files via upload

--- appall.py
+++ appall.py
@@ -15,9 +15,11 @@ from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
 # Standard scaler for preprocessing
 from sklearn.preprocessing import StandardScaler
 
+st.set_page_config(layout="wide")
+
 st.header("Forecasting Solar Photovoltaics (PV) Power Generation Using Machine Learning")
 
-st.subheader("Select a Machine Learning Model")
+#st.subheader("Select a Machine Learning Model")
 
 st.sidebar.image("Picture1.jpg", use_column_width=True)
 
@@ -33,6 +35,7 @@ with st.sidebar:
      st.subheader("Data Source")
      st.write('Buruthakanda Solar Park, Sri Lanka')
      st.write('Hourly data from 2011-07-21 to 2013-12-31')
+
 
 
 # Function for Multiple Linear Regression
@@ -82,7 +85,53 @@ def show_predict_MLR():
     col2.metric('R2',R2)
     col3.metric('MAE',MAE)
 
-    
+ # Function for Ridge Linear Regression
+def show_predict_RLR():
+# Load data
+    X_test = pd.read_csv('X_test.csv', index_col = 'Date', infer_datetime_format=True)
+    y_test = pd.read_csv('y_test.csv', index_col = 'Date', infer_datetime_format=True)
+
+# load saved model
+    with open('Ridge.pkl' , 'rb') as f:
+        rr = pickle.load(f)
+
+    st.subheader("Ridge Linear Regression")
+
+    st.markdown(f'<h1 style="color:#696969;font-size:22px;">{"Observed and Forecasted Power Generation"}</h1>', unsafe_allow_html=True)
+
+# plot the predictions for last 7 days of the test set
+    y_actualsel = y_test.loc['2013-12-23':'2014-01-01']
+    X_predsel = X_test.loc['2013-12-23':'2014-01-01']
+    y_actualplot = y_test.loc['2013-12-15':'2014-01-01']
+    y_predselrr = rr.predict(X_predsel)
+
+    fig,axes = plt.subplots(figsize = (15,7))
+    axes.plot(y_actualplot.index, y_actualplot, label='Observed')
+    axes.plot(y_actualsel.index, y_predselrr, color='y',  label='Forecast:RR')
+
+# set labels, legends and show plot
+    axes.set_xlabel('Date')
+    axes.set_ylabel('Power Generation in kW')
+    axes.set_xticks(np.arange(0, len(y_actualplot.index), 24))
+    axes.tick_params(axis='x',labelrotation = 90)
+    axes.legend()  
+
+    st.write(fig)  
+
+    st.markdown(f'<h1 style="color:#696969;font-size:22px;">{"Model Performance Matrices"}</h1>', unsafe_allow_html=True)
+
+# Performance evaluation
+    pred = rr.predict(X_test)
+
+    RMSE = round(np.sqrt(mean_squared_error(y_test, pred)),2)
+    R2 = round(r2_score(y_test, pred),2)
+    MAE = round(mean_absolute_error(y_test, pred),2)
+  
+    col1,col2,col3 = st.columns(3)
+    col1.metric('RMSE',RMSE)
+    col2.metric('R2',R2)
+    col3.metric('MAE',MAE)   
+
 # Functions for Elastic Net Regression
 def show_predict_ENR():
  
@@ -131,8 +180,65 @@ def show_predict_ENR():
     col2.metric('R2',R2_ENR)
     col3.metric('MAE',MAE_ENR)    
 
-page = st.selectbox("Select ML Model",("Multiple Linear Regression","Elastic Net Regression"))
+
+# Functions for Random Forest
+def show_predict_RF():
+
+# Load data
+    X_test_RF = pd.read_csv('X_test_lag_RF.csv', index_col = 'Date', infer_datetime_format=True)
+    y_test_RF = pd.read_csv('y_test_lag_RF.csv', index_col = 'Date', infer_datetime_format=True)
+
+# load saved model
+    with open('rflag.pkl' , 'rb') as f:
+        rfl= pickle.load(f)
+
+
+    st.subheader("Random Forest")
+
+    st.markdown(f'<h1 style="color:#696969;font-size:22px;">{"Observed and Forecasted Power Generation"}</h1>', unsafe_allow_html=True)
+
+# plot the predictions for last 7 days of the test set
+    X_predselrfl = X_test_RF.loc['2013-12-23':'2014-01-01']
+    y_predselrfl = rfl.predict(X_predselrfl)
+    y_actualsel = y_test_RF.loc['2013-12-23':'2014-01-01']
+    y_actualplot = y_test_RF.loc['2013-12-15':'2014-01-01']
+       
+    fig,axes = plt.subplots(figsize = (15,7))
+    axes.plot(y_actualplot.index, y_actualplot, label='Observed')
+    axes.plot(y_actualsel.index, y_predselrfl, color='g',  label='Forecast:RF')
+
+# set labels, legends and show plot
+    axes.set_xlabel('Date')
+    axes.set_ylabel('Power Generation in kW')
+    axes.set_xticks(np.arange(0, len(y_actualplot.index), 24))
+    axes.tick_params(axis='x',labelrotation = 90)
+    axes.legend()  
+
+    st.write(fig)  
+
+    st.markdown(f'<h1 style="color:#696969;font-size:22px;">{"Model Performance Matrices"}</h1>', unsafe_allow_html=True)
+
+# Performance evaluation
+    predrfl = rfl.predict(X_test_RF)
+
+    RMSE_RF = round(np.sqrt(mean_squared_error(y_test_RF, predrfl)),2)
+    R2_RF = round(r2_score(y_test_RF, predrfl),2)
+    MAE_RF = round(mean_absolute_error(y_test_RF, predrfl),2)
+  
+    col1,col2,col3 = st.columns(3)
+    col1.metric('RMSE',RMSE_RF)
+    col2.metric('R2',R2_RF)
+    col3.metric('MAE',MAE_RF)
+
+
+
+
+page = st.selectbox("Select a Machine Learning Model",("Multiple Linear Regression","Ridge Linear Regression","Elastic Net Regression","Random Forest"))
 if page == 'Multiple Linear Regression':
     show_predict_MLR()
-else:
+elif page == 'Ridge Linear Regression':
+    show_predict_RLR()
+elif page == 'Elastic Net Regression':
     show_predict_ENR()
+else:
+    show_predict_RF()
